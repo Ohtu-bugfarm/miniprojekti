15036c6294b8aa91f848fa0b07d4d5b1f29cb2f3
Lauri Suomi <lauri.suomi@helsinki.fi>
2015-04-10 17:55:49 +0300

    tarvis varmaan lisätä myös toimivuus

--- BugTeX/src/main/java/bugtex/kayttoliittyma/TekstiKayttoliittyma.java
+++ BugTeX/src/main/java/bugtex/kayttoliittyma/TekstiKayttoliittyma.java
@@ -1,9 +1,17 @@
 package bugtex.kayttoliittyma;
 
+import bugtex.komento.Komentotehdas;
+import bugtex.lukija.Syotteenlukija;
+
 public class TekstiKayttoliittyma implements Kayttoliittyma, Runnable {
 
+    private Syotteenlukija lukija = new Syotteenlukija();
+
     public void run() {
-        
+        Komentotehdas komennot = new Komentotehdas(lukija);
+        while (true) {
+            komennot.hae(lukija.lueRivi(">")).suorita();
+        }
     }
-    
+
 }
