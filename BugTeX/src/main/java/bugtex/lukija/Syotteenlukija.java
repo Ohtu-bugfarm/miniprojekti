2bee6f8b9e766886ea1cc813278458b8be72aea4
Lauri Suomi <lauri.suomi@helsinki.fi>
2015-04-10 15:35:59 +0300

    poistettiin turha metodi ja interfaci. Lisättiin rivinluku kysymyksellä

---
+++ BugTeX/src/main/java/bugtex/lukija/Syotteenlukija.java
@@ -0,0 +1,20 @@
+package bugtex.lukija;
+
+import java.util.Scanner;
+
+public class Syotteenlukija {
+
+    private Scanner lukija = new Scanner(System.in);
+   
+    public String lueRivi(String etuTeksti) {
+        System.out.print(etuTeksti + " ");
+        return lukija.nextLine();
+    }
+    
+    public String lueRiviKysymyksella(String etuTeksti, String kysymys) {
+        System.out.println(kysymys);
+        System.out.print(etuTeksti + " ");
+        return lukija.nextLine();
+    }
+
+}
--- BugTeX/src/main/java/lukija/Lukija.java
+++
@@ -1,6 +0,0 @@
-package lukija;
-
-public interface Lukija {
-    void tulosta(String teksti);
-    String lueRivi(String syote);
-}
