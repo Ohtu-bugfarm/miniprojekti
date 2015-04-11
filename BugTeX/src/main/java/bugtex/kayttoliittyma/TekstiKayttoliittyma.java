bf823d94788316e6c9af8ffce876c8a0a63eb572
Lauri Suomi <lauri.suomi@helsinki.fi>
2015-04-12 00:13:36 +0300

    Korjattiin ohjelman lopettamiseen liittynyt bugi

--- BugTeX/src/main/java/bugtex/kayttoliittyma/TekstiKayttoliittyma.java
+++ BugTeX/src/main/java/bugtex/kayttoliittyma/TekstiKayttoliittyma.java
@@ -8,20 +8,21 @@ public class TekstiKayttoliittyma implements Kayttoliittyma, Runnable {
 
     private final Lukija lukija;
     private final TietokantaRajapinta db;
-    
-    public TekstiKayttoliittyma(Lukija lukija, TietokantaRajapinta db){
+
+    public TekstiKayttoliittyma(Lukija lukija, TietokantaRajapinta db) {
         this.lukija = lukija;
         this.db = db;
     }
-    
+
     @Override
     public void run() {
         Komentotehdas komennot = new Komentotehdas(lukija, db);
         System.out.println("Tervetuloa käyttämään bugTexiä.");
-        
-        while (true) {
+
+        boolean running = true;
+        while (running) {
             String syote = lukija.lueRiviKysymyksella(">", "Anna komento");
-            if (syote == "Poistu") {
+            if (syote.equals("Poistu")) {
                 break;
             }
             komennot.hae(syote).suorita();
