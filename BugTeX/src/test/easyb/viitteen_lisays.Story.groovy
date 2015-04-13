a7888beb545e1349fd6ac3a7a2fff4708c2ca989
ejaasaari <elias.jaasaari@gmail.com>
2015-04-13 21:45:22 +0300

    Integraatiotestejä

--- BugTeX/src/test/easyb/viitteen_lisays.Story.groovy
+++ BugTeX/src/test/easyb/viitteen_lisays.Story.groovy
@@ -21,7 +21,23 @@ scenario 'oikealla komennolla käyttäjä pääsee lisäämään viitteen', {
     }
 }
 
-scenario 'virheellisellä komennolla käyttäjälle listataan käytettävät komennot', {
+scenario 'kirjaa ei lisätä jos käyttäjä keskeyttää toiminnon', {
+    given 'käyttäjä keskeyttää toiminnan kesken lisäyksen', {
+        db = new MuistiTietokanta()
+        lukija = new Valelukija("Lisaa", "Tekija", "Keskeyta")
+        ui = new TekstiKayttoliittyma(lukija, db)
+    }
+    
+    when 'toiminta keskeytetään', {
+        ui.run()
+    }
+    
+    then 'kirjan lisäys ei onnistu', {
+        lukija.getTulostukset().shouldNotHave("Kirjan lisäys onnistui")
+    }
+}
+
+scenario 'virheellisellä komennolla käyttäjälle listataan mahdolliset komennot', {
     given 'käyttäjä antaa komennon jota järjestelmä ei tunne', {
         db = new MuistiTietokanta()
         lukija = new Valelukija("Foo")
@@ -36,6 +52,3 @@ scenario 'virheellisellä komennolla käyttäjälle listataan käytettävät kom
         lukija.getTulostukset().shouldHave("Ohjelma tuntee komennot Lisaa, Poista, Tarkastele, Bibtex, Listaa, Poistu, Help")
     }
 }
-
-
-
---
+++ BugTeX/src/test/easyb/vitteiden_listaus.Story.groovy
@@ -0,0 +1,22 @@
+import bugtex.*;
+import bugtex.kayttoliittyma.*;
+import bugtex.tietokanta.*;
+import bugtex.IO.*;
+
+description 'Käyttäjä voi listata kaikki viitteet järjestelmässä'
+
+scenario 'oikealla komennolla käyttäjä pääsee listaamaan lisätyt viitteet', {
+    given 'annettu komento Lisaa', {
+        db = new MuistiTietokanta()
+        lukija = new Valelukija("Lisaa", "Tekija1", "Nimi", "Julkaisija", "Vuosi", "Listaa")
+        ui = new TekstiKayttoliittyma(lukija, db)
+    }
+
+    when 'tiedot on syötetty', {
+        ui.run()
+    }
+
+    then 'listaus onnistuu', {
+        lukija.getTulostukset().shouldHave("Tunnus: 1")
+    }
+}
