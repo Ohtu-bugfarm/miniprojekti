ee4dc7a7a166c2a99372843cdc945f3145091aef
ejaasaari <elias.jaasaari@gmail.com>
2015-04-28 22:15:51 +0300

    EasyB-testit konferenssijulkaisulle ja sekalaiselle viitteelle

--- BugTeX/src/test/easyb/viitteen_lisays.Story.groovy
+++ BugTeX/src/test/easyb/viitteen_lisays.Story.groovy
@@ -24,7 +24,7 @@ scenario 'oikealla komennolla käyttäjä pääsee lisäämään viitteen', {
 scenario 'Käyttäjä voi lisätä artikkeli viitteen', {
     given 'Käyttäjän antaessa lisää komennon artikkelille', {
         db = new MuistiTietokanta()
-        lukija = new Valelukija("lisaa", "artikkeli", "Marko", "Seikkailut", "Modern Times", "2000", "3", "poistu")
+        lukija = new Valelukija("lisaa", "artikkeli", "Marko", "Seikkailut", "Modern Times", "2000", "3")
         ui = new TekstiKayttoliittyma(lukija, db)
     }
 
@@ -37,3 +37,35 @@ scenario 'Käyttäjä voi lisätä artikkeli viitteen', {
     }
 }
 
+scenario 'Käyttäjä voi lisätä konferenssijulkaisu viitteen', {
+    given 'Käyttäjän antaessa lisää komennon konferenssijulkaisulle', {
+        db = new MuistiTietokanta()
+        lukija = new Valelukija("lisaa", "julkaisu", "Markon", "Hieno", "Julkaisu", "2000")
+        ui = new TekstiKayttoliittyma(lukija, db)
+    }
+
+    when 'ohjelma kysyy käyttäjältä artikkelin tiedot', {
+        ui.run()
+    }
+
+    then 'jonka jälkeen ohjelma tallentaa artikkelin ja ilmoittaa siitä käyttäjälle', {
+        lukija.getTulostukset().shouldHave("Viitteen lisäys onnistui")
+    }
+}
+
+scenario 'Käyttäjä voi lisätä sekalaisen viitteen', {
+    given 'Käyttäjän antaessa lisää komennon sekalaiselle viitteelle', {
+        db = new MuistiTietokanta()
+        lukija = new Valelukija("lisaa", "sekalainen", "Markon", "Hieno", "Hienosti julkaistu",
+                                "12", "2000", "muistutus")
+        ui = new TekstiKayttoliittyma(lukija, db)
+    }
+
+    when 'ohjelma kysyy käyttäjältä artikkelin tiedot', {
+        ui.run()
+    }
+
+    then 'jonka jälkeen ohjelma tallentaa artikkelin ja ilmoittaa siitä käyttäjälle', {
+        lukija.getTulostukset().shouldHave("Viitteen lisäys onnistui")
+    }
+}
